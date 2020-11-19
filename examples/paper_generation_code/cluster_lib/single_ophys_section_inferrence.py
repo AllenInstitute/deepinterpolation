3be25d9d9ee3fbaba6e93dab7505a38ad2a6ed61
Jerome Lecoq <jeromel@alleninstitute.org>
2020-11-18 18:05:09 -0800

    Add missing randomize parameter

--- examples/paper_generation_code/cluster_lib/single_ophys_section_inferrence.py
+++ examples/paper_generation_code/cluster_lib/single_ophys_section_inferrence.py
@@ -53,6 +53,7 @@ def main(argv):
     generator_param["start_frame"] = input_frames_start
     generator_param["end_frame"] = input_frames_end
     generator_param["movie_path"] = movie_path
+    generator_param["randomize"] = 0
 
     inferrence_param["type"] = "inferrence"
     inferrence_param["name"] = "core_inferrence"
