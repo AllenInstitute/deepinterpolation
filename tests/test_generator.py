70f8f7a3e64571105920e2f38a114809f2531645
Jerome Lecoq <jeromel@alleninstitute.org>
2021-04-29 15:24:42 -0700

    Add ephys test

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -38,3 +38,39 @@ def test_generator_tif_creation(tmp_path):
     data_generator = generator_obj.find_and_build()(path_generator)
 
     assert len(data_generator) == 8
+
+
+def test_generator_ephys_creation(tmp_path):
+    generator_param = {}
+
+    # We are reusing the data generator for training here. Some parameters like steps_per_epoch are irrelevant but currently needs to be provided
+    generator_param["type"] = "generator"
+    generator_param["name"] = "EphysGenerator"
+    generator_param["pre_post_frame"] = 30
+    generator_param["pre_post_omission"] = 1
+    generator_param[
+        "steps_per_epoch"
+    ] = -1  # No steps necessary for inference as epochs are not relevant. -1 deactivate it.
+
+    generator_param["train_path"] = os.path.join(
+        pathlib.Path(__file__).parent.absolute(),
+        "..",
+        "sample_data",
+        "ephys_tiny_continuous.dat2",
+    )
+
+    generator_param["batch_size"] = 10
+    generator_param["start_frame"] = 0
+    generator_param["end_frame"] = -1  # -1 to go until the end.
+    generator_param[
+        "randomize"
+    ] = 0  # This is important to keep the order and avoid the randomization used during training
+
+    path_generator = os.path.join(tmp_path, "generator.json")
+    json_obj = JsonSaver(generator_param)
+    json_obj.save_json(path_generator)
+
+    generator_obj = ClassLoader(path_generator)
+    data_generator = generator_obj.find_and_build()(path_generator)
+
+    assert len(data_generator) == 4996
