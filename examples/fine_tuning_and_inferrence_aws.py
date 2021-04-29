686eb7937478fc96110077cd0e64d4f38fb26bf7
Jerome Lecoq <jeromel@alleninstitute.org>
2021-04-29 09:55:03 -0700

    Add aws example training files

---
+++ examples/fine_tuning_and_inferrence_aws.py
@@ -0,0 +1,249 @@
+import deepinterpolation as de
+import sys
+from shutil import copyfile
+import os
+from deepinterpolation.generic import JsonSaver, ClassLoader
+import datetime
+from typing import Any, Dict
+import pathlib
+import sys, getopt
+import numpy as np
+
+
+def main(argv):
+    opts, args = getopt.getopt(
+        argv, [], ["movie_path=", "input_data_generator=", "raw_model_path=", "output_path_training=", "fine_tune=", 'batch_size=', 'training_loss=', 'learning_rate=',
+                   'steps_per_epoch=', 'save_raw=','pre_frame', 'post_frame', 'total_samples_for_training', 'total_samples_for_validation',
+                   'multi_gpus', 'nb_times_training_through_data', 'frame_start_validation', 'frame_end_validation', 'frame_start_training,
+                    'frame_end_training', 'frame_start_inferrence', 'frame_end_inferrence', 'output_file_inferrence    
+                   ],
+    )
+
+    # Default values    
+    batch_size = 5
+    save_raw = False
+    fine_tune = False
+    training_loss = "mean_squared_error"
+    learning_rate = 0.0001
+    steps_per_epoch = 200
+    pre_frame = 30
+    post_frame = 30
+    total_samples_for_training = 50000
+    total_samples_for_validation = 1000
+    multi_gpus = True
+    nb_times_training_through_data = 1
+    frame_start_validation = 0 
+    frame_end_validation = 6000
+    frame_start_training = 6001
+    frame_end_training = -1
+    frame_start_inferrence = 0
+    frame_end_inferrence = -1
+    output_file_inferrence = 
+    input_data_generator = "OphysGenerator"
+    for opt, arg in opts:
+        if opt == "--movie_path":
+            file_h5_path = arg
+        if opt == "--input_data_generator":
+            input_data_generator = arg
+        if opt == "--raw_model_path":
+            raw_model_path = arg
+        if opt == "--output_path_training":
+            output_path_training = arg
+        if opt == "--fine_tune":
+            fine_tune = bool(arg)
+        if opt == "--batch_size":
+            batch_size = int(arg)
+        if opt == "--training_loss":
+            training_loss = arg
+        if opt == "--learning_rate":
+            learning_rate = float(arg)
+        if opt == "--steps_per_epoch":
+            steps_per_epoch = int(arg)
+        if opt == "--save_raw":
+            save_raw = bool(arg)
+        if opt == "--pre_frame":
+            pre_frame = int(arg)
+        if opt == "--post_frame":
+            post_frame = int(arg)
+        if opt == "--total_samples_for_training":
+            total_samples_for_training = int(arg)
+        if opt == "--total_samples_for_validation":
+            total_samples_for_validation = int(arg)
+        if opt == "--multi_gpus":
+            multi_gpus = bool(arg)
+        if opt == "--nb_times_training_through_data":
+            nb_times_training_through_data = int(arg)
+        if opt == "--frame_start_validation":
+            frame_start_validation = int(arg)
+        if opt == "--frame_end_validation":
+            frame_end_validation = int(arg)
+        if opt == "--frame_start_training":
+            frame_start_training = int(arg)
+        if opt == "--frame_end_training":
+            frame_end_training = int(arg)
+        if opt == "--frame_start_inferrence":
+            frame_start_inferrence = int(arg)
+        if opt == "--frame_end_inferrence":
+            frame_end_inferrence = int(arg)
+        if opt == "--output_file_inferrence":
+            output_file_inferrence = arg
+             
+    # This is used for record-keeping
+    now = datetime.datetime.now()
+    run_uid = now.strftime("%Y_%m_%d_%H_%M")
+
+    # Initialize meta-parameters objects
+    training_param = {}
+    generator_param = {}
+    generator_test_param = {}
+
+    # An epoch is defined as the number of batches pulled from the dataset. Because our datasets are VERY large. Often, we cannot
+    # go through the entirity of the data so we define an epoch slightly differently than is usual.
+
+    generator_param["type"] = "generator"
+    generator_param["name"] = input_data_generator
+    generator_param["pre_frame"] = pre_frame
+    generator_param["post_frame"] = post_frame
+    generator_param["movie_path"] = movie_path
+    generator_param["batch_size"] = batch_size
+    generator_param["start_frame"] = frame_start_training
+    generator_param["end_frame"] = frame_end_training
+    generator_param["randomize"] = 1
+    generator_param["steps_per_epoch"] = steps_per_epoch
+    generator_param["total_samples"] = total_samples_for_training
+
+    generator_test_param["type"] = "generator"
+    generator_test_param["name"] = input_data_generator
+    generator_test_param["pre_frame"] = pre_frame
+    generator_test_param["post_frame"] = post_frame
+    generator_test_param["movie_path"] = movie_path
+    generator_test_param["batch_size"] = batch_size
+    generator_test_param["start_frame"] = frame_start_validation
+    generator_test_param["end_frame"] = frame_end_validation
+    generator_test_param["randomize"] = 1
+    generator_test_param["steps_per_epoch"] = -1
+    generator_test_param["total_samples"] = total_samples_for_validation
+
+    # Those are parameters used for the training process
+    training_param["type"] = "trainer"
+    training_param["name"] = "transfer_trainer"
+    training_param["run_uid"] = run_uid
+    training_param["model_path"] = raw_model_path
+
+    training_param["batch_size"] = batch_size
+    training_param["steps_per_epoch"] = steps_per_epoch
+    training_param[
+        "period_save"
+    ] = 1  # network model is potentially saved during training between a regular nb epochs
+    
+    training_param["nb_gpus"] = int(multi_gpus)
+    training_param["apply_learning_decay"] = 0
+    training_param[
+        "nb_times_through_data"
+    ] = nb_times_training_through_data  # if you want to cycle through the entire data. Two many iterations will cause noise overfitting
+    training_param["learning_rate"] = learning_rate
+    training_param["loss"] = training_loss
+    training_param[
+        "nb_workers"
+    ] = 16  # this is to enable multiple threads for data generator loading. Useful when this is slower than training
+
+    training_param["model_string"] = (
+        "transfer" + "_" + training_param["loss"] + "_" + training_param["run_uid"]
+    )
+
+    # Where do you store ongoing training progress
+    jobdir = output_path_training
+    training_param["output_dir"] = jobdir
+
+    try:
+        os.mkdir(jobdir)
+    except:
+        print("folder already exists")
+
+    # Here we create all json files that are fed to the training. This is used for recording purposes as well as input to the
+    # training process
+    path_training = os.path.join(jobdir, "training.json")
+    json_obj = JsonSaver(training_param)
+    json_obj.save_json(path_training)
+
+    path_generator = os.path.join(jobdir, "generator.json")
+    json_obj = JsonSaver(generator_param)
+    json_obj.save_json(path_generator)
+
+    path_test_generator = os.path.join(jobdir, "test_generator.json")
+    json_obj = JsonSaver(generator_test_param)
+    json_obj.save_json(path_test_generator)
+
+    # We find the generator obj in the collection using the json file
+    generator_obj = ClassLoader(path_generator)
+    generator_test_obj = ClassLoader(path_test_generator)
+
+    # We find the training obj in the collection using the json file
+    trainer_obj = ClassLoader(path_training)
+
+    # We build the generators object. This will, among other things, calculate normalizing parameters.
+    train_generator = generator_obj.find_and_build()(path_generator)
+    test_generator = generator_test_obj.find_and_build()(path_test_generator)
+
+    # We build the training object.
+    training_class = trainer_obj.find_and_build()(
+        train_generator, test_generator, path_training
+    )
+
+    # Start training. This can take very long time.
+    training_class.run()
+
+    # Finalize and save output of the training.
+    training_class.finalize()
+    
+    
+    
+    
+    # Now we go into inferrence 
+    generator_param = {}
+    inferrence_param = {}
+
+    generator_param["type"] = "generator"
+    generator_param["name"] = input_data_generator
+    generator_param["pre_frame"] = pre_frame
+    generator_param["post_frame"] = post_frame
+
+    # This is meant to allow compatibility with a generator also used in training
+    generator_param["steps_per_epoch"] = steps_per_epoch
+
+    generator_param["batch_size"] = batch_size
+    generator_param["start_frame"] = frame_start_inferrence
+    generator_param["end_frame"] = frame_end_inferrence
+    generator_param["movie_path"] = movie_path
+    generator_param["randomize"] = 0
+
+    inferrence_param["type"] = "inferrence"
+    inferrence_param["name"] = "core_inferrence"
+    
+    if fine_tune:
+        inferrence_param["model_path"] = training_class.output_model_file_path
+    else:
+        inferrence_param["model_path"] = raw_model_path
+        
+    inferrence_param["output_file"] = output_file_inferrence
+    inferrence_param["save_raw"] = save_raw
+
+    while NotDone:
+        path_generator = output_file + ".generator.json"
+        json_obj = JsonSaver(generator_param)
+        json_obj.save_json(path_generator)
+
+        path_infer = output_file + ".inferrence.json"
+        json_obj = JsonSaver(inferrence_param)
+        json_obj.save_json(path_infer)
+
+        generator_obj = ClassLoader(path_generator)
+        data_generator = generator_obj.find_and_build()(path_generator)
+
+        inferrence_obj = ClassLoader(path_infer)
+        inferrence_class = inferrence_obj.find_and_build()(path_infer, data_generator)
+
+        inferrence_class.run()
+        
+if __name__ == "__main__":
+    main(sys.argv[1:])
---
+++ examples/fine_tuning_aws.py
@@ -0,0 +1,186 @@
+import deepinterpolation as de
+from shutil import copyfile
+import os
+from deepinterpolation.generic import JsonSaver, ClassLoader
+import datetime
+from typing import Any, Dict
+import pathlib
+import numpy as np
+import argparse
+import json
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+
+    # input data and model directories
+    parser.add_argument('--raw_model_path', type=str,
+                        default=os.environ['SM_CHANNEL_MODEL'])
+    parser.add_argument('--raw_data_path', type=str,
+                        default=os.environ['SM_CHANNEL_TRAIN'])
+    parser.add_argument('--val_data_path', type=str,
+                        default=os.environ['SM_CHANNEL_TEST'])
+    parser.add_argument('--nb_gpus', type=str,
+                        default=os.environ['SM_NUM_GPUS'])
+    parser.add_argument('--output_path', type=str,
+                        default=os.environ['SM_MODEL_DIR'])
+
+    # hyperparameters sent by the client are passed as
+    # command-line arguments to the script.
+    parser.add_argument('--batch_size', type=int, default=5)
+    parser.add_argument('--training_loss', type=str,
+                        default="mean_squared_error")
+    parser.add_argument('--learning_rate', type=float, default=0.0001)
+    parser.add_argument('--steps_per_epoch', type=int, default=200)
+    parser.add_argument('--pre_frame', type=int, default=30)
+    parser.add_argument('--post_frame', type=int, default=30)
+    parser.add_argument('--total_samples_for_training',
+                        type=int, default=500)
+    parser.add_argument('--total_samples_for_validation',
+                        type=int, default=100)
+    parser.add_argument('--nb_times_training_through_data',
+                        type=int, default=1)
+    parser.add_argument('--frame_start_validation', type=int, default=0)
+    parser.add_argument('--frame_end_validation', type=int, default=6000)
+    parser.add_argument('--frame_start_training', type=int, default=6001)
+    parser.add_argument('--frame_end_training', type=int, default=-1)
+    parser.add_argument('--input_data_generator',
+                        type=str, default="OphysGenerator")
+
+    args, _ = parser.parse_known_args()
+
+    # Passing on the parameters
+    raw_model_path  args.raw_model_path
+    raw_data_path = args.raw_data_path
+    val_data_path = args.val_data_path
+    output_path = args.output_path
+    nb_gpus = args.nb_gpus
+
+    batch_size = args.batch_size
+    training_loss = args.training_loss
+    learning_rate = args.learning_rate
+    steps_per_epoch = args.steps_per_epoch
+    pre_frame = args.pre_frame
+    post_frame = args.post_frame
+    total_samples_for_training = args.total_samples_for_training
+    total_samples_for_validation = args.total_samples_for_validation
+    nb_times_training_through_data = args.nb_times_training_through_data
+    frame_start_validation = args.frame_start_validation
+    frame_end_validation = args.frame_end_validation
+    frame_start_training = args.frame_start_training
+    frame_end_training = args.frame_end_training
+    frame_start_inferrence = args.frame_start_inferrence
+    frame_end_inferrence = args.frame_end_inferrence
+
+    # This is used for record-keeping
+    now = datetime.datetime.now()
+    run_uid = now.strftime("%Y_%m_%d_%H_%M")
+
+    # Initialize meta-parameters objects
+    training_param = {}
+    generator_param = {}
+    generator_test_param = {}
+
+    # An epoch is defined as the number of batches pulled from the dataset.
+    # Because our datasets are VERY large. Often, we cannot
+    # go through the entirity of the data so we define an
+    # epoch slightly differently than is usual.
+
+    generator_param["type"] = "generator"
+    generator_param["name"] = input_data_generator
+    generator_param["pre_frame"] = pre_frame
+    generator_param["post_frame"] = post_frame
+    generator_param["movie_path"] = raw_data_path
+    generator_param["batch_size"] = batch_size
+    generator_param["start_frame"] = frame_start_training
+    generator_param["end_frame"] = frame_end_training
+    generator_param["randomize"] = 1
+    generator_param["steps_per_epoch"] = steps_per_epoch
+    generator_param["total_samples"] = total_samples_for_training
+
+    generator_test_param["type"] = "generator"
+    generator_test_param["name"] = input_data_generator
+    generator_test_param["pre_frame"] = pre_frame
+    generator_test_param["post_frame"] = post_frame
+    generator_test_param["movie_path"] = val_data_path
+    generator_test_param["batch_size"] = batch_size
+    generator_test_param["start_frame"] = frame_start_validation
+    generator_test_param["end_frame"] = frame_end_validation
+    generator_test_param["randomize"] = 1
+    generator_test_param["steps_per_epoch"] = -1
+    generator_test_param["total_samples"] = total_samples_for_validation
+
+    # Those are parameters used for the training process
+    training_param["type"] = "trainer"
+    training_param["name"] = "transfer_trainer"
+    training_param["run_uid"] = run_uid
+    training_param["model_path"] = raw_model_path
+
+    training_param["batch_size"] = batch_size
+    training_param["steps_per_epoch"] = steps_per_epoch
+    training_param[
+        "period_save"
+    ] = 1
+    # network model is potentially saved during training
+    # between a regular nb epochs
+
+    training_param["nb_gpus"] = nb_gpus
+    training_param["apply_learning_decay"] = 0
+    training_param[
+        "nb_times_through_data"
+    ] = nb_times_training_through_data
+    # if you want to cycle through the entire data.
+    # Two many iterations will cause noise overfitting
+    training_param["learning_rate"] = learning_rate
+    training_param["loss"] = training_loss
+    training_param[
+        "nb_workers"
+    ] = 16
+    # this is to enable multiple threads for data generator loading.
+    # Useful when this is slower than training
+
+    training_param["model_string"] = (
+        "transfer" + "_" +
+        training_param["loss"] + "_" + training_param["run_uid"]
+    )
+
+    # Where do you store ongoing training progress
+    training_param["output_dir"] = output_path
+
+    # Here we create all json files that are fed to the training.
+    # This is used for recording purposes as well as input to the
+    # training process
+    path_training = os.path.join(output_path, "training.json")
+    json_obj = JsonSaver(training_param)
+    json_obj.save_json(path_training)
+
+    path_generator = os.path.join(output_path, "generator.json")
+    json_obj = JsonSaver(generator_param)
+    json_obj.save_json(path_generator)
+
+    path_test_generator = os.path.join(output_path, "test_generator.json")
+    json_obj = JsonSaver(generator_test_param)
+    json_obj.save_json(path_test_generator)
+
+    # We find the generator obj in the collection using the json file
+    generator_obj = ClassLoader(path_generator)
+    generator_test_obj = ClassLoader(path_test_generator)
+
+    # We find the training obj in the collection using the json file
+    trainer_obj = ClassLoader(path_training)
+
+    # We build the generators object. This will, among other things,
+    # calculate normalizing parameters.
+    train_generator = generator_obj.find_and_build()(path_generator)
+    test_generator = generator_test_obj.find_and_build()(path_test_generator)
+
+    # We build the training object.
+    training_class = trainer_obj.find_and_build()(
+        train_generator, test_generator, path_training
+    )
+
+    # Start training. This can take very long time.
+    training_class.run()
+
+    # Finalize and save output of the training.
+    training_class.finalize()
