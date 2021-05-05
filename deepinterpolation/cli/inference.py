6a050b043d8ea3bd30455bef896993409576547f
Dan <danielk@alleninstitute.org>
2021-05-05 09:29:20 -0700

    modularize and comment temporary normalization step

--- deepinterpolation/cli/inference.py
+++ deepinterpolation/cli/inference.py
@@ -8,6 +8,48 @@ from deepinterpolation.cli.schemas import InferenceInputSchema
 from deepinterpolation.generic import ClassLoader
 
 
+def normalize_uint16_output(input_file: Path,
+                            output_file: Path) -> np.ndarray:
+    """function to globally normalize and convert datatype of output
+    movies. The output movie is shifted to be >=0 and scaled to the
+    same max value as the input movie. The movie is zero-padded at the
+    end to match the number of frames of the input movie.
+
+    Parameters
+    ----------
+    input_file: Path
+        the pre-denoising movie. Used to establish histogram bounds
+        of intensity.
+    output_file: Path
+        the post-denoising movie. It is floating point and needs to
+        be scaled and type converted
+
+    Returns
+    -------
+    data: np.ndarray
+        the scaled and type converted result
+
+    Notes
+    -----
+    We anticipate removing this when the Inference class is upgraded to
+    take care of negative values and convert to uint16.
+
+    """
+    with h5py.File(input_file, "r") as f:
+        inmax = f["data"][()].max()
+        inshape = f["data"].shape
+
+    with h5py.File(output_file, "r") as f:
+        out = f["data"][()].squeeze()
+
+    out = (out - out.min()) * inmax / out.ptp()
+    out = out.astype('uint16')
+    nextra = inshape[0] - out.shape[0]
+    dextra = np.zeros((nextra, *out.shape[1:]), dtype='uint16')
+    out = np.concatenate((out, dextra), axis=0)
+    return out
+
+
 class Inference(argschema.ArgSchemaParser):
     default_schema = InferenceInputSchema
 
@@ -50,24 +92,12 @@ class Inference(argschema.ArgSchemaParser):
         # so that the CLI works with tiff, dat, ... files.
         if '.h5' in self.args["generator_params"]["train_path"]:
             self.logger.info("fixing up the range and shape of the result")
-            with h5py.File(self.args["generator_params"]["train_path"], "r") \
-                    as f:
-                dmax = f["data"][()].max()
-                dshape = f["data"].shape
-
-            with h5py.File(
-                    self.args["inference_params"]["output_file"], "r") as f:
-                d = f["data"][()].squeeze()
-
-            d = (d - d.min()) * dmax / d.ptp()
-            d = d.astype('uint16')
-            nextra = dshape[0] - d.shape[0]
-            dextra = np.zeros((nextra, *d.shape[1:]), dtype='uint16')
-            d = np.concatenate((d, dextra), axis=0)
-
+            data = normalize_uint16_output(
+                    Path(self.args["generator_params"]["train_path"]),
+                    Path(self.args["inference_params"]["output_file"]))
             with h5py.File(
                     self.args["inference_params"]["output_file"], "w") as f:
-                f.create_dataset("data", data=d)
+                f.create_dataset("data", data=data)
             self.logger.info(
                 f"wrote {self.args['inference_params']['output_file']}")
 
