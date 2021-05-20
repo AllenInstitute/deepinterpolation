757b05a0bfd1e31d9f30fecd51ec2f7761e91dd6
Jerome Lecoq <jeromel@alleninstitute.org>
2021-05-20 14:45:00 -0700

    Add naomi training code

---
+++ examples/paper_generation_code/2021-05-18-cluster_fine_tune_charact.py
@@ -0,0 +1,84 @@
+import os
+from pbstools import PythonJob
+import datetime
+
+folder_path = r"/allen/programs/braintv/workgroups/neuralcoding/2p_data/"\
+    "single_plane/2021-05-14-simulation-naomi/"
+
+raw_path_folder = r"/allen/programs/braintv/workgroups/neuralcoding/2p_data/"\
+    "single_plane/2021-05-14-simulation-naomi/"
+
+list_exp_folder = [
+    'typicalVolume80mW',
+    'typicalVolume160mW',
+    'sparseLabelingVolume'
+]
+
+raw_model = r"/allen/programs/braintv/workgroups/ophysdev/OPhysCore/Deep2p/"\
+    "unet_single_1024_mean_absolute_error_Ai93_2019_09_11_23_32/"\
+    "2019_09_11_23_32_unet_single_1024_mean_absolute_error_Ai93-0450.h5"
+
+python_file = os.path.join(
+    "/home/jeromel/Documents/Projects/Deep2P/repos/deepinterpolation/"
+    "examples/paper_generation_code",
+    "2021-05-18-fine_tune_tif_charact.py",
+)
+
+for indiv_id in list_exp_folder:
+    path_tif = os.path.join(
+        raw_path_folder, indiv_id, indiv_id + "noisy.tif")
+
+    local_output_path = os.path.join(
+        folder_path, indiv_id, 'deepinterp')
+
+    try:
+        os.mkdir(local_output_path)
+    except Exception:
+        print("folder already exists")
+
+    now = datetime.datetime.now()
+    run_uid = now.strftime("%Y_%m_%d_%H_%M_%S_%f")
+
+    jobdir = local_output_path
+
+    output_terminal = os.path.join(
+        jobdir, run_uid +
+        os.path.basename(python_file) + "_running_terminal.txt"
+    )
+
+    job_settings = {
+        "queue": "braintv",
+        "mem": "250g",
+        "walltime": "200:00:00",
+        "ppn": 16,
+        "gpus": 1,
+    }
+
+    job_settings.update(
+        {
+            "outfile": os.path.join(jobdir, "$PBS_JOBID.out"),
+            "errfile": os.path.join(jobdir, "$PBS_JOBID.err"),
+            "email": "jeromel@alleninstitute.org",
+            "email_options": "a",
+        }
+    )
+
+    arg_to_pass = (
+        " --file_h5_path "
+        + path_tif
+        + " --raw_model_path "
+        + raw_model
+        + " --output_path "
+        + local_output_path
+    )
+
+    PythonJob(
+        python_file,
+        python_executable="/allen/programs/braintv/workgroups/nc-ophys/"
+        "Jeromel/conda/tf20-env/bin/python",
+        conda_env="/allen/programs/braintv/workgroups/nc-ophys/Jeromel/"
+        "conda/tf20-env",
+        jobname="tf_" + os.path.basename(python_file),
+        python_args=arg_to_pass + " > " + output_terminal,
+        **job_settings
+    ).run(dryrun=False)
---
+++ examples/paper_generation_code/2021-05-18-cluster_oephys_inference.py
@@ -0,0 +1,118 @@
+import os
+from pbstools import PythonJob
+from shutil import copyfile
+import datetime
+import glob
+
+raw_path_folder = r"/allen/programs/braintv/workgroups/neuralcoding/2p_data/"\
+    "single_plane/2021-05-14-simulation-naomi/"
+
+output_path = r"/allen/programs/braintv/workgroups/neuralcoding/2p_data/"\
+    "single_plane/2021-05-14-simulation-naomi/"
+
+list_exp_folder = [
+    'typicalVolume80mW',
+    'typicalVolume160mW',
+    'sparseLabelingVolume'
+]
+
+for indiv_id in list_exp_folder:
+    try:
+        path_tif = os.path.join(
+            raw_path_folder, indiv_id, indiv_id + "noisy.tif")
+
+        jobdir = os.path.join(output_path, indiv_id, 'deepinterp', 'infer')
+
+        try:
+            os.mkdir(jobdir)
+        except Exception:
+            print("folder already exists")
+
+        raw_model = os.path.join(output_path, indiv_id, 'deepinterp',
+                                 "*_model.h5")
+        raw_model = glob.glob(raw_model)[0]
+
+        python_file = r"/home/jeromel/Documents/Projects/Deep2P/repos/"\
+            "deepinterpolation/examples/paper_generation_code/cluster_lib/"\
+            "single_tif_section_inferrence.py"
+
+        model_file = raw_model
+
+        pre_post_frame = 30
+        pre_post_omission = 0
+
+        now = datetime.datetime.now()
+        run_uid = now.strftime("%Y_%m_%d_%H_%M")
+
+        start_frame = 0
+        end_frame = -1
+        batch_size = 5
+        save_raw = True
+        output_type = "float32"
+        x_size = 512
+        y_size = 512
+
+        output_terminal = os.path.join(
+            jobdir, run_uid + "_running_terminal.txt")
+        script_basename = os.path.basename(__file__)
+        copyfile(
+            os.path.realpath(__file__), os.path.join(
+                jobdir, run_uid + "_" + script_basename)
+        )
+
+        local_path = os.path.join(
+            jobdir, "movie_" + os.path.basename(model_file))
+
+        job_settings = {
+            "queue": "braintv",
+            "mem": "180g",
+            "walltime": "24:00:00",
+            "ppn": 16,
+        }
+
+        job_settings.update(
+            {
+                "outfile": os.path.join(jobdir, "$PBS_JOBID.out"),
+                "errfile": os.path.join(jobdir, "$PBS_JOBID.err"),
+                "email": "jeromel@alleninstitute.org",
+                "email_options": "a",
+            }
+        )
+
+        arg_to_pass = [
+            "--movie_path "
+            + path_tif
+            + " --frame_start "
+            + str(start_frame)
+            + " --frame_end "
+            + str(end_frame)
+            + " --output_file "
+            + local_path
+            + " --model_file "
+            + model_file
+            + " --batch_size "
+            + str(batch_size)
+            + " --pre_post_frame "
+            + str(pre_post_frame)
+            + " --pre_post_omission "
+            + str(pre_post_omission)
+            + " --save_raw "
+            + str(save_raw)
+            + " --x_size "
+            + str(x_size)
+            + " --y_size "
+            + str(y_size)
+        ]
+
+        PythonJob(
+            python_file,
+            python_executable="/allen/programs/braintv/workgroups/nc-ophys/"
+            "Jeromel/conda/tf20-env/bin/python",
+            conda_env="/allen/programs/braintv/workgroups/nc-ophys/Jeromel/"
+            "conda/tf20-env",
+            jobname="movie_2p",
+            python_args=arg_to_pass[0] + " > " + output_terminal,
+            **job_settings
+        ).run(dryrun=False)
+    except Exception:
+        print("experiment associated could not be found")
