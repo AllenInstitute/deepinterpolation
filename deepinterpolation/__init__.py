f043ee7cf24b32505f44ad4fce8f8d5495adab46
Mike Huang <huangjmike@gmail.com>
2023-01-30 09:04:30 -0800

    add option to install intel-tensorflow-avx512 if supported

--- deepinterpolation/__init__.py
+++ deepinterpolation/__init__.py
@@ -1,2 +1,2 @@
 # Version of deepinterpolation package. Should match the version in setup.py
-__version__ = "0.1.5"
+__version__ = "0.1.6"
--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from setuptools import setup, find_packages
-
+import platform
+import os
 
 with open("README.rst", encoding='utf-8') as f:
     readme = f.read()
@@ -9,12 +10,20 @@ with open("README.rst", encoding='utf-8') as f:
 with open("LICENSE") as f:
     license = f.read()
 
-with open("requirements.txt", "r") as f:
-    required = f.read().splitlines()
+if platform.system == 'Linux' or platform.system == 'Darwin':
+    if os.system("grep avx512 /proc/cpuinfo") == 0:
+        with open("requirements-avx512.txt", "r") as f:
+            required = f.read().splitlines()
+    else:
+        with open("requirements.txt", "r") as f:
+            required = f.read().splitlines()
+else:
+    with open("requirements.txt", "r") as f:
+        required = f.read().splitlines()
 
 setup(
     name="deepinterpolation",
-    version="0.1.5",
+    version="0.1.6",
     description="Implemenent DeepInterpolation to denoise data by removing \
 independent noise",
     long_description_content_type='text/x-rst',
