50d3c914d4d8df96d45ba270b9587dbd050ae8e4
Mike Huang <huangjmike@gmail.com>
2023-02-14 10:11:14 -0800

    use only one requirements.txt

--- setup.py
+++ setup.py
@@ -10,20 +10,18 @@ with open("README.rst", encoding='utf-8') as f:
 with open("LICENSE") as f:
     license = f.read()
 
+with open("requirements.txt", "r") as f:
+    required = f.read().splitlines()
 if platform.system == 'Linux' or platform.system == 'Darwin':
     if os.system("grep avx512 /proc/cpuinfo") == 0:
-        with open("requirements-avx512.txt", "r") as f:
-            required = f.read().splitlines()
-    else:
-        with open("requirements.txt", "r") as f:
-            required = f.read().splitlines()
-else:
-    with open("requirements.txt", "r") as f:
-        required = f.read().splitlines()
+        for i, pkg in enumerate(required):
+            pkg = pkg.split("==")
+            if pkg[0] == "tensorflow":
+                pkg[0] = "intel-tensorflow-avx512"
+            required[i] = "==".join(pkg)
 
 setup(
     name="deepinterpolation",
-    version="0.1.6",
     description="Implemenent DeepInterpolation to denoise data by removing \
 independent noise",
     long_description_content_type='text/x-rst',
